a05a56238124a31f6de38f8bf4de4019bd93c670
Paul Savoie <paul@savoie.at>
2024-10-18 17:08:32 +0200

    properly adds sample folder and fixes policy reference

---
+++ sample/sample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  sample
+//
+//  Created by Paul Savoie on 18.10.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sample/sample/sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleApp.swift
+//  sample
+//
+//  Created by Paul Savoie on 18.10.24.
+//
+
+import SwiftUI
+
+@main
+struct sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
